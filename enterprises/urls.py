4fd7a247b73e0d0b8b7a6aa5d97d3e8584b1be4b
Heather Duke <heathernoelduke@gmail.com>
2023-04-20 19:43:31 -0400

    Port project to Django.

---
+++ enterprises/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about-evan/', views.about, name='about'),
+    path('lessons/', views.lessons, name='lessons'),
+    path('business-consultations/', views.business, name='business'),
+    path('performances/', views.performances, name='performances'),
+    path('recordings/', views.recordings, name='recordings'),
+    path('research-projects/', views.research, name='research'),
+    path('contact/', views.contact, name='contact'),
+]
---
+++ enterprises/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "enterprises/index.html")
+
+
+def about(request):
+    return render(request, 'enterprises/about.html')
+
+
+def lessons(request):
+    return render(request, 'enterprises/lessons.html')
+
+
+def business(request):
+    return render(request, 'enterprises/business.html')
+
+
+def performances(request):
+    return render(request, 'enterprises/performances.html')
+
+
+def recordings(request):
+    return render(request, 'enterprises/recordings.html')
+
+
+def research(request):
+    return render(request, 'enterprises/research.html')
+
+
+def contact(request):
+    return render(request, 'enterprises/contact.html')
