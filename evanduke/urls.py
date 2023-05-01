13f1203ca2910bb051557ebc22f258c5b1b0466d
Heather Duke <heathernoelduke@gmail.com>
2023-05-01 14:47:29 -0400

    Update docker settings

--- evanduke/urls.py
+++ evanduke/urls.py
@@ -23,3 +23,7 @@ urlpatterns = [
     path('', include('enterprises.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
