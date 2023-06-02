d49b8888b7972b197a170c380fa90056021c8718
Heather Duke <heathernoelduke@gmail.com>
2023-06-02 18:30:07 -0400

    Add script to automate deployment to production

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Checking for optional tag. If absent, the tag will default to \"latest\""
+TAG=${1:-latest}
+
+echo "Pulling updated app image..."
+docker pull hnduke/evanduke:$TAG
+
+echo "Taking old containers down..."
+docker compose down
+
+echo "Spinning up new containers..."
+docker compose up -d
+
+echo "Removing old images..."
+# remove unused images
+docker image prune -f
+
+echo "Deployed."
