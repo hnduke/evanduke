b1217dd18918d24899788f49fc995898a796da94
Heather Duke <heathernoelduke@gmail.com>
2023-06-07 09:10:20 -0400

    Remove all pages except index, about, and contact

--- enterprises/urls.py
+++ enterprises/urls.py
@@ -5,9 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about-evan/', views.about, name='about'),
-    path('lessons/', views.lessons, name='lessons'),
-    path('business-consultations/', views.business, name='business'),
-    path('performances/', views.performances, name='performances'),
-    path('research-projects/', views.research, name='research'),
     path('contact/', views.contact, name='contact'),
 ]
--- enterprises/views.py
+++ enterprises/views.py
@@ -9,21 +9,5 @@ def about(request):
     return render(request, 'enterprises/about.html')
 
 
-def lessons(request):
-    return render(request, 'enterprises/lessons.html')
-
-
-def business(request):
-    return render(request, 'enterprises/business.html')
-
-
-def performances(request):
-    return render(request, 'enterprises/performances.html')
-
-
-def research(request):
-    return render(request, 'enterprises/research.html')
-
-
 def contact(request):
     return render(request, 'enterprises/contact.html')
