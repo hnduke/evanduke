388ed3ed9749556ee89cd54389985e51d615e8e3
Heather Duke <heathernoelduke@gmail.com>
2023-08-28 06:20:38 -0400

    Add reCAPTCHA tests

--- enterprises/tests.py
+++ enterprises/tests.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 
 from enterprises.models import FrequentlyAskedQuestion, ServiceType
+from enterprises.recaptcha import is_human
 
 
 @pytest.fixture
@@ -45,3 +46,36 @@ def test_faqs_view_with_invalid_service_type(client):
     url = reverse("faqs", kwargs={"service_type": "INVALID_TYPE"})
     response = client.get(url)
     assert response.status_code == 404
+
+
+def test_invalid_token_properties(mocker):
+    mock_create_assessment = mocker.patch("enterprises.recaptcha.create_assessment")
+    mock_create_assessment.return_value.token_properties.valid = False
+    result = is_human("test_token", "test_action")
+    assert result is False
+
+
+def test_action_mismatch(mocker):
+    mock_create_assessment = mocker.patch("enterprises.recaptcha.create_assessment")
+    mock_create_assessment.return_value.token_properties.valid = True
+    mock_create_assessment.return_value.token_properties.action = "wrong_action"
+    result = is_human("test_token", "test_action")
+    assert result is False
+
+
+def test_score_below_threshold(mocker):
+    mock_create_assessment = mocker.patch("enterprises.recaptcha.create_assessment")
+    mock_create_assessment.return_value.token_properties.valid = True
+    mock_create_assessment.return_value.token_properties.action = "test_action"
+    mock_create_assessment.return_value.risk_analysis.score = 0.5
+    result = is_human("test_token", "test_action")
+    assert result is False
+
+
+def test_all_checks_pass(mocker):
+    mock_create_assessment = mocker.patch("enterprises.recaptcha.create_assessment")
+    mock_create_assessment.return_value.token_properties.valid = True
+    mock_create_assessment.return_value.token_properties.action = "test_action"
+    mock_create_assessment.return_value.risk_analysis.score = 0.9
+    result = is_human("test_token", "test_action")
+    assert result is True
