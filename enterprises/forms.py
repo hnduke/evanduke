fb30a92c767d70631790b6fb67ff5ec4e71a8674
Heather Duke <heathernoelduke@gmail.com>
2023-08-29 19:45:54 -0400

    Move reCAPTCHA validation to form (with tests)

--- enterprises/forms.py
+++ enterprises/forms.py
@@ -1,10 +1,16 @@
 from django import forms
+from django.core.exceptions import BadRequest, ValidationError
 
 from enterprises.models import ContactFormSubmission
+from enterprises.recaptcha import is_human
+
+SUBMISSION_CHAR_LIMIT = 1000
 
 
 class ContactForm(forms.ModelForm):
     """A simple comment form."""
+
+    action = "contact"
 
     class Meta:
         model = ContactFormSubmission
@@ -15,5 +21,37 @@ class ContactForm(forms.ModelForm):
             "submission": "Comment",
         }
         widgets = {
-            "submission": forms.Textarea(attrs={"rows": 5, "maxlength": 2000}),
+            "submission": forms.Textarea(
+                attrs={"rows": 5, "maxlength": SUBMISSION_CHAR_LIMIT}
+            ),
         }
+
+    def clean_submission(self):
+        submission = self.cleaned_data.get("submission")
+        if submission and len(submission) <= SUBMISSION_CHAR_LIMIT:
+            return submission
+        raise ValidationError(
+            f"The maximum number of characters allowed is {SUBMISSION_CHAR_LIMIT}"
+        )
+
+    def clean(self):
+        """Check the reCAPTCHA in addition to everything else.
+
+        Raises BadRequest if reCAPTCHA concludes that something is fishy.
+        """
+
+        super().clean()
+        captcha = self.data.get("g-recaptcha-response")
+        try:
+            valid = is_human(captcha, self.action)
+        except Exception as e:
+            # TODO: log e
+            self.add_error(
+                "submission",
+                "We are sorry, but we're experiencing some difficulties with our provider just now. Please "
+                "try again in a few minutes.",
+            )
+            return
+
+        if not valid:
+            raise BadRequest
---
+++ tests/test_forms.py
@@ -0,0 +1,52 @@
+import pytest
+
+from enterprises.forms import SUBMISSION_CHAR_LIMIT, BadRequest, ContactForm
+
+
+@pytest.fixture
+def valid_data():
+    return {
+        "name": "John Doe",
+        "reply_email": "john.doe@example.com",
+        "submission": "Hello, World!",
+    }
+
+
+def test_valid_form_submission(mocker, valid_data):
+    mock_is_human = mocker.patch("enterprises.forms.is_human")
+    mock_is_human.return_value = True
+    form = ContactForm(data=valid_data)
+    assert form.is_valid()
+
+
+def test_invalid_form_submission_length(mocker, valid_data):
+    mock_is_human = mocker.patch("enterprises.forms.is_human")
+    mock_is_human.return_value = True
+    valid_data["submission"] = "x" * (SUBMISSION_CHAR_LIMIT + 1)
+    form = ContactForm(data=valid_data)
+    assert not form.is_valid()
+    assert "submission" in form.errors
+
+
+def test_valid_form_submission_empty_name_field(mocker, valid_data):
+    mock_is_human = mocker.patch("enterprises.forms.is_human")
+    mock_is_human.return_value = True
+    valid_data["name"] = ""
+    form = ContactForm(data=valid_data)
+    assert form.is_valid()  # name is optional
+
+
+def test_recaptcha_fail(mocker, valid_data):
+    mock_is_human = mocker.patch("enterprises.forms.is_human")
+    mock_is_human.return_value = False
+    form = ContactForm(data=valid_data)
+    with pytest.raises(BadRequest):
+        form.is_valid()
+
+
+def test_recaptcha_exception(mocker, valid_data):
+    mock_is_human = mocker.patch("enterprises.forms.is_human")
+    mock_is_human.side_effect = Exception("Random Exception")
+    form = ContactForm(data=valid_data)
+    assert not form.is_valid()
+    assert "submission" in form.errors
