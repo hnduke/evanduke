364984e3b5b424dca841160df1de8c3a7d64910d
Heather Duke <heathernoelduke@gmail.com>
2023-09-04 14:21:07 -0400

    Add Sentry monitoring

--- evanduke/settings.py
+++ evanduke/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env()
 
@@ -186,3 +188,21 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
+# Monitoring:
+if env("ENVIRONMENT", default="") == "production":
+    sentry_sdk.init(
+        dsn=env("SENTRY_DSN", default=""),
+        integrations=[DjangoIntegration()],
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
