7fd30008e134ac41a3bd2e39d736754559ca724c
Heather Duke <heathernoelduke@gmail.com>
2023-09-04 14:28:28 -0400

    Sentry PII is not necessary

--- evanduke/settings.py
+++ evanduke/settings.py
@@ -196,7 +196,7 @@ if env("ENVIRONMENT", default="") == "production":
         integrations=[DjangoIntegration()],
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True,
+        # send_default_pii=True,
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production.
