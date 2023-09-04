35610061b9ff3029bfbc564a0c8aa1da857a6473
Heather Duke <heathernoelduke@gmail.com>
2023-09-04 12:22:10 -0400

    Update urls

--- evanduke/urls.py
+++ evanduke/urls.py
@@ -20,10 +20,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("enterprises.urls")),
     path("admin/", admin.site.urls),
+    path("", include("enterprises.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
